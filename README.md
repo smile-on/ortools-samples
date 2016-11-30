082128de2246e014d13720933949fd91aabb2ded
kochepasov.viatcheslav <vkochepasov@gmail.com>
2016-11-30 10:37:32 -0500

    fixed compilation error; documented how to run a sample.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ortools-samples
+# OR-Tools Samples
+
+Java is not favorite language among supported by [Google's or-tools project](https://developers.google.com/optimization/). This repo is a collection of `Java` samples to facilitate learning of or-tools project.
+The project source code has two places of Java examples:
+ * Java **samples** in main
+[or-tools github repository](https://github.com/google/or-tools/tree/master/examples/com/google/ortools/samples)
+ * Java **tutorial examples** are [limited to a few](https://github.com/google/or-tools/tree/master/documentation/tutorials/java/com/google/ortools/tutorial).
+
+## Running samples
+Users are welcome to use IDE of their choice but no preferences are carried over here. Any sample in this repository can be run by command line:
+```
+$ javac @options.arg src\samples\SimpleRoutingTest.java
+$ java -cp "out;lib\*" -Djava.library.path=lib\rt  samples.SimpleRoutingTest
+```
+
--- src/samples/SimpleRoutingTest.java
+++ src/samples/SimpleRoutingTest.java
@@ -1,86 +1,86 @@
 //package com.google.ortools.samples;
 package samples;
 
-import java.util.ArrayList;
-
 import com.google.ortools.constraintsolver.Assignment;
 import com.google.ortools.constraintsolver.NodeEvaluator2;
 import com.google.ortools.constraintsolver.RoutingModel;
 
+import java.util.ArrayList;
+
 public class SimpleRoutingTest {
-
-  //Static Add Library
-  static { System.loadLibrary("jniortools"); }
-
-  private ArrayList<Integer> globalRes;
-  private long globalResCost;
-  private int[][] costMatrix;
-
-  public ArrayList<Integer> getGlobalRes() {return globalRes;}
-  public void setGlobalRes(ArrayList<Integer> globalRes) {this.globalRes = globalRes;}
-  public long getGlobalResCost() {return globalResCost;}
-  public void setGlobalResCost(int globalResCost) {this.globalResCost = globalResCost;}
-  public int[][] getCostMatrix() {return costMatrix;}
-  public void setCostMatrix(int[][] costMatrix) {this.costMatrix = costMatrix;}
-
-  public SimpleRoutingTest(int[][] costMatrix) {
-    super();
-    this.costMatrix = costMatrix;
-    globalRes = new ArrayList();
-  }
-
-  //Node Distance Evaluation
-  public static class NodeDistance extends NodeEvaluator2 {
-    private int[][] costMatrix;
-
-    public NodeDistance(int[][] costMatrix) {
-      this.costMatrix = costMatrix;
+    //Static Add Library
+    static {
+        System.loadLibrary("jniortools");
     }
 
-    @Override
-    public long run(int firstIndex, int secondIndex) {
-      return costMatrix[firstIndex][secondIndex];
+    public static void main(String[] args) throws Exception {
+        int[][] costMatrix = new int[4][4];
+        costMatrix[0][0] = 0;
+        costMatrix[0][1] = 5;
+        costMatrix[0][2] = 3;
+        costMatrix[0][3] = 6;
+        costMatrix[1][0] = 5;
+        costMatrix[1][1] = 0;
+        costMatrix[1][2] = 8;
+        costMatrix[1][3] = 1;
+        costMatrix[2][0] = 3;
+        costMatrix[2][1] = 8;
+        costMatrix[2][2] = 0;
+        costMatrix[2][3] = 4;
+        costMatrix[3][0] = 6;
+        costMatrix[3][1] = 1;
+        costMatrix[3][2] = 4;
+        costMatrix[3][3] = 0;
+        SimpleRoutingTest model = new SimpleRoutingTest(costMatrix);
+        model.solve();
+        model.reportSolution();
     }
-  }
 
-  //Solve Method
-  public void solve() {
-    RoutingModel routing = new RoutingModel(costMatrix.length, 1, 0);
-    routing.setFirstSolutionStrategy(RoutingModel.ROUTING_PATH_CHEAPEST_ARC);
-    NodeDistance distances = new NodeDistance(costMatrix);
-    routing.setCost(distances);
+    // model's environment
+    int locationCount;
+    NodeEvaluator2 distancesCallback;
+    // solution
+    ArrayList<Integer> globalRes;
+    long globalResCost;
 
-    Assignment solution = routing.solve();
-    if (solution != null) {
-      int route_number = 0;
-      for (long node = routing.start(route_number); !routing.isEnd(node); node = solution.value(routing.nextVar(node))) {
-        globalRes.add((int) node);
-      }
+    public SimpleRoutingTest(int[][] costMatrix) {
+        distancesCallback = new NodeDistance(costMatrix);
+        locationCount = costMatrix.length;
     }
-    globalResCost = solution.objectiveValue();
-    System.out.println("cost = " + globalResCost);
-  }
 
+    //Solve Method
+    public void solve() {
+        RoutingModel routing = new RoutingModel(locationCount, 1, 0);
+//    routing.setFirstSolutionStrategy(RoutingModel.ROUTING_PATH_CHEAPEST_ARC);
+        routing.setCost(distancesCallback);
 
-  public static void main(String[] args) throws Exception {
-    int[][] values = new int[4][4];
-    values[0][0]=0;
-    values[0][1]=5;
-    values[0][2]=3;
-    values[0][3]=6;
-    values[1][0]=5;
-    values[1][1]=0;
-    values[1][2]=8;
-    values[1][3]=1;
-    values[2][0]=3;
-    values[2][1]=8;
-    values[2][2]=0;
-    values[2][3]=4;
-    values[3][0]=6;
-    values[3][1]=1;
-    values[3][2]=4;
-    values[3][3]=0;
-    SimpleRoutingTest model = new SimpleRoutingTest(values);
-    model.solve();
-  }
+        Assignment solution = routing.solve();
+        if (solution != null) {
+            globalRes = new ArrayList();
+            int route_number = 0;
+            for (long node = routing.start(route_number); !routing.isEnd(node); node = solution.value(routing.nextVar(node))) {
+                globalRes.add((int) node);
+            }
+        }
+        globalResCost = solution.objectiveValue();
+    }
+
+    public void reportSolution() {
+        System.out.println(String.format("cost %d solution %s", globalResCost, globalRes));
+    }
+
+    //Node Distance Evaluation
+    public static class NodeDistance extends NodeEvaluator2 {
+        private int[][] costMatrix;
+
+        public NodeDistance(int[][] costMatrix) {
+            this.costMatrix = costMatrix;
+        }
+
+        @Override
+        public long run(int firstIndex, int secondIndex) {
+            return costMatrix[firstIndex][secondIndex];
+        }
+    }
+
 }
